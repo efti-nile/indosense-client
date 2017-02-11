49646f33454164172329eebfc9b1fd36fb9de6f7
efti-nile <efti.nile@gmail.com>
2017-02-11 22:38:19 +0400

    1. Added saving to dedicated folder 2. Added monitoring mod

--- indosense-client.py
+++ indosense-client.py
@@ -1,6 +1,10 @@
-import serial, openpyxl, time, msvcrt
+import serial, openpyxl, time, msvcrt, os
 
 track_recording = True
+monitoring = True
+
+MATLAB_FOLDER = 'E:/work/indosens/'
+MATLAB_SCRIPT_NAME = 'static_meas.m'
 
 # Commands
 SET_REG = b'02'
@@ -24,19 +28,19 @@ port = serial.Serial('COM' + str(default_com_no), baudrate)
 port.flush()
 
 port.write(SET_REG + ALT_CONFIG + b'01' + b'\r') # Swith off Rp module
-print(port.read(32)[8])
+#print(port.read(32)[8])
 
 port.write(SET_REG + D_CONFIG + b'01' + b'\r') # Don't track amplitude
-print(port.read(32)[8])
+#print(port.read(32)[8])
 
-port.write(SET_REG + DIG_CONF + b'F0' + b'\r') # F_min = 8MHz
-print(port.read(32)[8])
+#port.write(SET_REG + DIG_CONF + b'F0' + b'\r') # F_min = 8MHz
+#print(port.read(32)[8])
 
 port.write(SET_REG + LHR_CONFIG + b'03' + b'\r') # F_div = 8
-print(port.read(32)[8])
+#print(port.read(32)[8])
 
 port.write(SET_REG + START_CONFIG + b'00' + b'\r') # Continuous conversion mode
-print(port.read(32)[8])
+#print(port.read(32)[8])
 
 def read_ldata():
     global port
@@ -60,20 +64,61 @@ def kbfunc():
         ret = False
     return ret
 
-while track_recording:
-	raw_data = []
-	filename = input('Enter file name: ')
-	input('Press enter to star...')
-	print('Press \'s\' to stop')
-	while True:
-		raw_data.append(read_ldata())
-		time.sleep(0.05)
+while True:
+	mode = input('Input \'m\' to enter monitor mod.\nInput \'r\' to enter monitor mod.\n')
+	
+	if mode == 'm':
+		monitoring = True
+		recording = False
+		print('Press \'s\' to stop')
+	elif mode == 'r':
+		monitoring = False
+		recording = True
+	else:
+		continue
+	
+	while monitoring:	
+		time.sleep(0.2)
+		print('LHR_DATA = ' + str(read_ldata()))
 		key = kbfunc()
 		if key != False:
 			if key == b's':
 				break;
-	wb = openpyxl.Workbook()
-	ws = wb.active
-	for i in range(len(raw_data)):
-		ws.append([raw_data[i]])
-	wb.save(filename + '.xlsx')
+	
+	if recording:
+		raw_data = []
+		throw_away_the_record = False
+		filename = input('Enter output excel file name (without \'.xlsx\'): ')
+		base_folder = os.path.join(MATLAB_FOLDER, filename)
+		excel_file = os.path.join(base_folder, filename + '.xlsx')
+		if os.path.exists(excel_file):
+			ans = input('File already exists. Replace? (y/n) :')
+			if ans == 'y':
+				os.remove(excel_file)	
+			else:
+				continue
+		input('Press enter to star...')
+		print('Press \'s\' to stop\nPress \'d\' to throw away file')
+		count = 0	
+		port.flush()
+		while True:
+			raw_data.append(read_ldata())
+			time.sleep(0.15)
+			key = kbfunc()
+			count = count + 1
+			if count % 5 == 0:
+				print('LHR_DATA = ' + str(raw_data[-1]))
+			if key != False:
+				if key == b's':
+					break
+				elif key == b'd':
+					throw_away_the_record = True
+					break
+		if not throw_away_the_record:
+			if not os.path.isdir(base_folder):
+				os.mkdir(base_folder)
+			wb = openpyxl.Workbook()
+			ws = wb.active
+			for i in range(len(raw_data)):
+				ws.append([raw_data[i]])
+			wb.save(excel_file)
